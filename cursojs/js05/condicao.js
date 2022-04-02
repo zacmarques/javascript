92976ee0a6ef3da19e0e76e5d9805d58596c49ff
zacmarques <isaacmarquesg@outlook.com>
2022-04-01 21:33:11 -0300

    JavaScript att

---
+++ cursojs/js05/condicao.js
@@ -0,0 +1,7 @@
+console.log('Tenha acesso à sua situação abaixo!')
+var vel = Number(console.count('Digite qual foi sua velocidade'))
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel >= 60){
+    console.log(`Você ultrapassou a velocidade permitida! MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
