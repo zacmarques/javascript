65d7701333a43ae933ab75d06940d045433bb64f
zacmarques <isaacmarquesg@outlook.com>
2022-04-06 14:49:41 -0300

    Update script2.js

--- cursojs/js06/script2.js
+++ cursojs/js06/script2.js
@@ -8,16 +8,38 @@ function calcular(){
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gener = '';
-        var estadom = '';
-        var estadof = '';
+        var idadef = idade.value
+        var gener = ''
+        var estadomc = 'Criança'; //estado masculino criança
+        var estadomj = 'Jovem';   //estado masculino jovem
+        var estadoma = 'Adulto';  //estado masculino adulto
+        var estadomi = 'Idoso';   //estado masculino idoso
+        var estadofc = 'Criança'; //estado masculino criança
+        var estadofj = 'Jovem';   //estado masculino jovem
+        var estadofa = 'Adulta';  //estado masculino adulta
+        var estadofi = 'Idosa';   //estado masculino idosa
         if (fsex[0].checked){
-            gener = Masculino
-            estadom = idade > 0 && idade < 12? 'Criança': idade < 21? 'Jovem': idade < 65? 'Adulto': 'Idoso';
+            gener = 'Masculino'
+            if(idadef > 0 && idadef < 12){
+                res.innerHTML = `<p>Você é <strong>${estadomc}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            } else if(idadef < 21){
+                res.innerHTML = `<p>Você é <strong>${estadomj}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            } else if (idadef < 65){
+                res.innerHTML = `<p>Você é <strong>${estadoma}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            } else (idadef > 65);{
+                res.innerHTML = `<p>Você é <strong>${estadomi}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            }
         } else if(fsex[1].checked){
-            gener = Feminino
-            estadof = idade > 0 && idade < 12? 'Criança': idade < 21? 'Jovem': idade < 65? 'Adulta': 'Idosa';
+            gener = 'Feminino'
+            if(idadef > 0 && idadef < 12){
+                res.innerHTML = `<p>Você é <strong>${estadofc}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            } else if(idadef < 21){
+                res.innerHTML = `<p>Você é <strong>${estadofj}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            } else if (idadef < 65){
+                res.innerHTML = `<p>Você é <strong>${estadofa}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            } else (idadef > 65);{
+                res.innerHTML = `<p>Você é <strong>${estadofi}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
+            }
         }
-        res.innerHTML = `<p>Você é <strong>${estado}</strong> do gênero <strong>${gener}</strong> e a sua idade é: <strong>${idade} anos</strong>!</p>`
     }
 }
