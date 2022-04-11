05c4bf2893ce5833323f81e7748ddc319b36ad22
zacmarques <isaacmarquesg@outlook.com>
2022-04-11 16:37:21 -0300

    Script da Introdução à Repetição

---
+++ cursojs/js07/script.js
@@ -0,0 +1,25 @@
+var c = 1
+var n = 1
+for(var c = 1; c <= 100; c++, n++){
+    console.log(`Esse é o número C: ${c} e o N: ${n}.`)
+}
+/*
+Repetição com teste no início
+while(c <= 500){
+    console.log('Tudo bem?' + n)
+    c++
+    n++
+}
+
+Repetição com teste no final
+do{
+    console.log(`Esse é o número ${n}.`)
+    c++
+    n++    
+} while (c <=250)
+
+Repetição com teste de controle (o mais utilizado pela maioria, é o mais completo na minha opinião)
+for(var c = 1; c <= 100; c++, n++){
+    console.log(`Esse é o número C: ${c} e o N: ${n}.`)
+}
+}*/
